16f2f172a26e7460e48f47428e63afabdb18ed12
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-24 16:29:30 +0200

    Initial commit

---
+++ src/EvenNumberSum.java
@@ -0,0 +1,5 @@
+public class EvenNumberSum {
+    public static void main(String[] args) {
+        System.out.println("This program counts the Sum of all even numbers between 1 and 100");
+    }
+}
