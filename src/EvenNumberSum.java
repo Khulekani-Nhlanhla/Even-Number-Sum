5b91d56875da609d7fd402a6f1c7702fb41ec681
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-24 17:13:37 +0200

    I have added a for loop in order to keep the program counting from 1 to 100.I have also nested a if statement inside the loop meant to find the Sum of the even numbers.

--- src/EvenNumberSum.java
+++ src/EvenNumberSum.java
@@ -1,5 +1,17 @@
 public class EvenNumberSum {
     public static void main(String[] args) {
         System.out.println("This program counts the Sum of all even numbers between 1 and 100");
+        int SumOfEven = 0;
+        for (int i=0;i<101;i++){
+            if(i % 2 == 0){
+                SumOfEven = SumOfEven + i;
+
+            }
+
+
+        }
+        System.out.println("The sum of the all the even numbers between 1 and 100 are: "+SumOfEven);
+
+
     }
 }
